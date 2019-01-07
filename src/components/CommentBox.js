fd6d4dc0b84adf2bf83f122a15efd8db40290dbf
Kyle Wang <kyle@Kyles-MacBook-Pro.local>
2019-01-06 17:40:41 -0800

    update more tests, abs path, and .env

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>Comment List</div>;
-}
+class CommentBox extends Component {
+	state = { comment: '' };
+
+	handleChange = (event) => {
+		this.setState({comment: event.target.value});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		// TODO - Call and action creator
+		// And Save the Comment
+
+		this.setState({ comment: ''});
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h4> Add a Comment</h4>
+				<textarea onChange={this.handleChange} value={this.state.comment}/>
+				<div>
+					<button>Submit a Comment</button>
+				</div>
+			</form>
+		)
+	}
+};
+
+export default CommentBox;
